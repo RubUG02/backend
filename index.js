068d3175f91add7f3e20222ba96a60e642df69b1
unknown <contact@fastweb.com>
2020-11-18 23:19:38 -0600

    AVANCES

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+//Crear servidor Express
+const app = express();
+
+//Rutas
+app.get('/',(req, res) => {
+    console.log("Se recibió una petición en /");
+    res.json({ 
+        ok:true,
+        msg:"Recibido",
+     });
+}
+);
+
+//Configuracion del puerto
+app.listen(4000, () => {
+    console.log('Backend corriendo en el puerto ${4000}');
+})
